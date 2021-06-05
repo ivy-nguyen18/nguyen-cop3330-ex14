239d7e03577a2271e06baf5d01dc0b570db74eae
ivy-nguyen18 <nguyen.ivy1802@knights.ucf.edu>
2021-06-05 12:25:02 -0400

    Initial commit

---
+++ src/main/java/ex14/base/App.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+package ex14.base;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is the order amount? ");
+        double amount = input.nextDouble();
+
+        System.out.print("What is the state? ");
+        String state = input.next();
+
+        Tax tax = new Tax(amount);
+        System.out.println(tax.stateTax(state));
+
+
+    }
+}
---
+++ src/main/java/ex14/base/Tax.java
@@ -0,0 +1,24 @@
+package ex14.base;
+
+public class Tax {
+    private double amount;
+    static final double WI_TAX = 5.5 / 100;
+
+    public Tax(double amount){
+        this.amount = amount;
+    }
+
+    public StringBuilder stateTax(String state2){
+
+        StringBuilder output = new StringBuilder();
+
+        if("WI".equals(state2)){
+            output.append(String.format("The subtotal is $%.2f.%nThe tax is $%.2f.%n", amount, amount * WI_TAX));
+            amount += amount * WI_TAX;
+        }
+
+        output.append(String.format("The total is $%.2f.", amount));
+
+        return output;
+    }
+}
